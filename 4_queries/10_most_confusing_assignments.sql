717827a0922d80b06e4a0fee7950b33007701b57
Ramya Adimoolam <ramyaa1431@gmail.com>
2021-01-05 22:02:56 +0000

    queries

---
+++ 4_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,9 @@
+SELECT assignments.id as id,
+ assignments.name as name, 
+ assignments.day as day, 
+ assignments.chapter as chapter, 
+ COUNT(assistance_requests.id) as total_requests
+FROM assistance_requests
+JOIN assignments ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
---
+++ 4_queries/13_teacher_and_total_assistances.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT teachers.name as teacher,
+ cohorts.name as cohort,
+ count(assistance_requests.*) as  total_assistances 
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE assistance_requests.teacher_id = teachers.id 
+AND cohorts.name = 'JUL02'
+GROUP BY teachers.name,cohorts.name
+ORDER BY teacher;
